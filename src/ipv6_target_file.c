86f8c9ab6c4c9c931617c56f02c129c5bcc095f0
Le Gai <110470025+LeGai325@users.noreply.github.com>
2026-03-11 16:08:57 +0800

    Update ipv6_target_file.c

--- src/ipv6_target_file.c
+++ src/ipv6_target_file.c
@@ -9,7 +9,6 @@
 
 #include <arpa/inet.h>
 #include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -19,18 +18,6 @@
 #define LOGGER_NAME "ipv6_target_file"
 
 static FILE *fp;
-
-static char *trim(char *s)
-{
-	while (*s == ' ' || *s == '\t' || *s == '\r') {
-		s++;
-	}
-	char *end = s + strlen(s);
-	while (end > s && (end[-1] == ' ' || end[-1] == '\t' || end[-1] == '\r')) {
-		*--end = '\0';
-	}
-	return s;
-}
 
 int ipv6_target_file_init(char *file)
 {
@@ -48,57 +35,29 @@ int ipv6_target_file_init(char *file)
 	return 0;
 }
 
-int ipv6_target_file_get_target(struct in_addr *dst4, bool *has_dst4,
-			      struct in6_addr *dst6)
+int ipv6_target_file_get_ipv6(struct in6_addr *dst)
 {
-	// ipv6_target_file_init() needs to be called before ipv6_target_file_get_target()
+    // ipv6_target_file_init() needs to be called before ipv6_target_file_get_ipv6()
 	assert(fp);
 
-	char line[256];
+	char line[100];
 
-	while (fgets(line, sizeof(line), fp) != NULL) {
+	if (fgets(line, sizeof(line), fp) != NULL) {
 		// Remove newline
 		char *pos;
 		if ((pos = strchr(line, '\n')) != NULL) {
 			*pos = '\0';
 		}
-		if (line[0] == '\0' || line[0] == '#') {
-			continue;
+		int rc = inet_pton(AF_INET6, line, dst);
+		if (rc != 1) {
+			log_fatal(LOGGER_NAME, "could not parse IPv6 address from line: %s: %s", line, strerror(errno));
+			return 1;
 		}
-		if (strcmp(line, "ipv4,ipv6") == 0) {
-			continue;
-		}
-
-		char *comma = strchr(line, ',');
-		if (comma) {
-			*comma = '\0';
-			char *ipv4_str = trim(line);
-			char *ipv6_str = trim(comma + 1);
-			int rc4 = inet_pton(AF_INET, ipv4_str, dst4);
-			int rc6 = inet_pton(AF_INET6, ipv6_str, dst6);
-			if (rc4 != 1 || rc6 != 1) {
-				log_fatal(LOGGER_NAME,
-					  "could not parse IPv4/IPv6 pair from line: %s,%s",
-					  ipv4_str, ipv6_str);
-				return 1;
-			}
-			*has_dst4 = true;
-		} else {
-			char *ipv6_str = trim(line);
-			int rc6 = inet_pton(AF_INET6, ipv6_str, dst6);
-			if (rc6 != 1) {
-				log_fatal(LOGGER_NAME,
-					  "could not parse IPv6 address from line: %s",
-					  ipv6_str);
-				return 1;
-			}
-			dst4->s_addr = 0;
-			*has_dst4 = false;
-		}
-		return 0;
+	} else {
+		return 1;
 	}
 
-	return 1;
+	return 0;
 }
 
 int ipv6_target_file_deinit()
